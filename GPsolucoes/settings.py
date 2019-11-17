50a12d2fd210358a66470f1e216da335878fd481
MyNr <pedroh.mix@gmail.com>
2019-11-17 00:14:36 -0300

    Adicionando arquivo

--- GPsolucoes/settings.py
+++ GPsolucoes/settings.py
@@ -111,8 +111,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 VALI_CONFIG = {
     # the vali-admin themes  default, blue, purple, green,brown
